6837ae1f5040393a6c38ed3fec03e413aa6a05b5
Pradeep772 <97424653+Pradeep772@users.noreply.github.com>
2022-05-02 21:41:36 +0530

    #include<stdio.h> int main() {     int a,b,m;     scanf("%d%d",&a,&b);     m=(a>b)?a:b;     while(1)     {         if(m%a==0&&m%b==0)         {             printf("%d",m);             break;         }         ++m;     } }

---
+++ LCM.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,m;
+    scanf("%d%d",&a,&b);
+    m=(a>b)?a:b;
+    while(1)
+    {
+        if(m%a==0&&m%b==0)
+        {
+            printf("%d",m);
+            break;
+        }
+        ++m;
+    }
+}
