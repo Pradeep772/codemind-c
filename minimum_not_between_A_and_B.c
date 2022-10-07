8d68b9b38cec7d5e2f4cf062bc6a50997da00243
Pradeep772 <97424653+Pradeep772@users.noreply.github.com>
2022-10-07 15:20:55 +0530

    #include<stdio.h>
    int main()
    
    {
    
        int n,arr[100],i,r1,r2,min=0,f=0;
    
        scanf("%d",&n);
    
        for(i=0;i<n;i++)
    
        scanf("%d",&arr[i]);
    
        //for(i=0;i<n;i++)
    
        //printf("%d ",arr[i]);
    
        scanf("%d%d",&r1,&r2);
    
       // printf("
    %d %d",r1,r2);
    
        for(i=0;i<n;i++)
    
        {
    
            if(arr[i]<r1 || arr[i]>r2)
    
            {
    
                f=1;
    
                min=arr[i];
    
                break;
    
            }
    
        }
    
        if(f==0)
    
        {
    
            printf("-1");
    
            return 0;
    
        }
    
        for(i=0;i<n;i++)
    
        {
    
            if(arr[i]<r1 || arr[i]>r2)
    
            {
    
                if(min>arr[i])
    
                min=arr[i];
    
            }
    
        }
    
        printf("%d",min);
    
        return 0;
    
    }

---
+++ minimum_not_between_A_and_B.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+    int n,arr[100],i,r1,r2,min=0,f=0;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+
+    scanf("%d",&arr[i]);
+
+    //for(i=0;i<n;i++)
+
+    //printf("%d ",arr[i]);
+
+    scanf("%d%d",&r1,&r2);
+
+   // printf("
+%d %d",r1,r2);
+
+    for(i=0;i<n;i++)
+
+    {
+
+        if(arr[i]<r1 || arr[i]>r2)
+
+        {
+
+            f=1;
+
+            min=arr[i];
+
+            break;
+
+        }
+
+    }
+
+    if(f==0)
+
+    {
+
+        printf("-1");
+
+        return 0;
+
+    }
+
+    for(i=0;i<n;i++)
+
+    {
+
+        if(arr[i]<r1 || arr[i]>r2)
+
+        {
+
+            if(min>arr[i])
+
+            min=arr[i];
+
+        }
+
+    }
+
+    printf("%d",min);
+
+    return 0;
+
+}
